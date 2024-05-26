3f3333c7dae5cb2537e749c0de8ae9c1da8c8643
enihsgnir <enihsgnir@gmail.com>
2024-05-26 20:01:20 +0900

    refactor message struct and action types

--- websocket/message.go
+++ websocket/message.go
@@ -4,15 +4,10 @@ const (
 	JoinRoomAction  = "JOIN_ROOM"
 	LeaveRoomAction = "LEAVE_ROOM"
 
-	SendTextAction  = "SEND_TEXT"
-	SendVoiceAction = "SEND_VOICE"
-	SendVideoAction = "SEND_VIDEO"
+	SendTextAction = "SEND_TEXT"
 
 	MuteAction   = "MUTE"
 	UnmuteAction = "UNMUTE"
-
-	DeafenAction   = "DEAFEN"
-	UndeafenAction = "UNDEAFEN"
 
 	TurnOnCamAction  = "TURN_ON_CAM"
 	TurnOffCamAction = "TURN_OFF_CAM"
@@ -22,5 +17,5 @@ type Message struct {
 	ChatroomID int    `json:"chatroomId"`
 	SenderID   int    `json:"senderId"`
 	Action     string `json:"action"`
-	Data       string `json:"data,omitempty"`
+	Content    string `json:"content,omitempty"`
 }
